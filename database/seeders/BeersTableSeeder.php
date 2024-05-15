e908f90a14d49c60ef830639eff8e44cf1f13273
luigidibella <lykos.omega@gmail.com>
2024-05-15 12:10:05 +0200

    [add] seeder

---
+++ database/seeders/BeersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BeersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
